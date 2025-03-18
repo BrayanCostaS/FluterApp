5ddb67e11c6c483e0e6c095e0e02a357276be214
brayan.costa32 <brayan.costa32@gmail.com>
2025-03-18 01:30:30 +0000

    Alterações Validas

--- test/dart_test.dart
+++ test/dart_test.dart
@@ -11,6 +11,25 @@ bool isPerfect(int num) {
   return sum == num;
 }
 
+int factorial(int n) {
+  if (n < 0) throw ArgumentError('Número deve ser não negativo.');
+  int result = 1;
+  int i = n;
+  while (i > 1) {
+    result *= i;
+    i--;
+  }
+  return result;
+}
+
+bool isPrime(int num) {
+  return false;
+}
+
+int sumOfDigits(int num) {
+  return num;
+}
+
 void main() {
   group('Testes de Número perfeito', () {
     test('Número perfeito 6', () {
@@ -22,28 +41,31 @@ void main() {
     });
   });
 
-  group('Testes de Soma dos dígitos', () {
-    test('Soma dos dígitos de 123', () {
-      expect(sumOfDigits(123), equals(6));
+  group('Testes de Fatorial', () {
+    test('Fatorial de 5', () {
+      expect(factorial(5), 120);
     });
-    test('Soma dos dígitos de -1', () {
-      expect(sumOfDigits(-1), throwsArgumentError);
+
+    test('Fatorial de número negativo deve lançar erro', () {
+      expect(() => factorial(-3), throwsArgumentError);
     });
   });
 
-  bool isPrime(int num) {
-    // Verifica se o número é menor que 2, caso seja, não é primo
-    if (num <= 1) {
-      return false;
-    }
+  group('Testes de Número primo', () {
+   test('Número primo 7', () {
+     expect(isPrime(7), isTrue);
+   });
+   test('Número não primo 10', () {
+     expect(isPrime(10), isFalse);
+   });
+ });
 
-    // Verifica se o número é divisível por qualquer número entre 2 e a raiz quadrada do número
-    for (int i = 2; i <= num ~/ 2; i++) {
-      if (num % i == 0) {
-        return false; // Se divisível, não é primo
-      }
-    }
-
-    return true; // Caso não seja divisível por nenhum número, é primo
-  }
+ group('Testes de Soma dos dígitos', () {
+   test('Soma dos dígitos de 123', () {
+     expect(sumOfDigits(123), equals(6));
+   });
+   test('Soma dos dígitos de -1', () {
+     expect(sumOfDigits(-1), throwsArgumentError);
+   });
+ });
 }
